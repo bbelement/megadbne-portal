58c124fcec17e50da0f698da0b1511f6cff8334a
Brian Barrus <bb@studio-element.com>
2013-01-27 20:23:16 -0700

    Megadyne Portal

---
+++ megadyne-portal.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style>
+h1 {font-family:Arial, Helvetica, sans-serif;
+color:#222;
+text-shadow: 0px 2px 3px #999;
+font-size:36px;	
+}
+h4 {font-family:Arial, Helvetica, sans-serif;
+color:#222;
+letter-spacing:3px;
+text-shadow: 0px 2px 3px #999;
+font-size:18px;
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Element Megadyne Portal</title>
+</head>
+
+<body style="background-image:url(images/leather.jpg)">
+<div style="width:1000px; margin-left:auto; margin-right:auto; margin-top:20px;">
+
+<img style="float:left; margin-top:-13px; margin-left:-15px;" src="images/e3d-m.png"/>
+<h1> Element Megadyne Portal</h1>
+
+<div style="background-color:white; opacity:.85; width:1000px; height:1000px; border-radius:9px; -webkit-box-shadow: 0px 3px 7px #000; box-shadow: 0px 0px 7px #000;">
+<div style="float:left; overflow: hidden;"><iframe src="http://personal.crocodoc.com/PRU9ra6?embedded=true" width="300" height="100%" frameborder="0" ></iframe></div>
+<iframe src="https://www.google.com/calendar/embed?src=qj17vviepbkoft9t55aakaiti0%40group.calendar.google.com&ctz=America/Denver" style="border: 0; margin-top:5px;" width="700" height="600" frameborder="0" scrolling="no" ></iframe>
+
+
+
+</div>
+
+<div style="margin-left:auto; margin-right:auto;"><h4>STUDIO-ELEMENT</h4></div>
+</div>
+</body>
+</html>
